208d0549942f5a1441cc0c2a32a6b15f116b74d3
Lex <al.vl.shiyan@gmail.com>
2016-06-25 00:45:13 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,159 @@
+// $(document).ready() заменяется на...
+document.addEventListener( "DOMContentLoaded", function() {
+	
+	// Переменные для отображения числа на главном дисплее и результирующего выражения на втором дисплее
+	var number_display, expression_display;
+	
+	var round_type = 2;
+	
+	// Функция сброса переменных, отображающихся на дисплее
+	function reset() { number_display = "0"; expression_display = ""; };
+	
+	// Функция удаления последнего символа из числа
+	function deleteLast() { number_display = number_display.substring(0, number_display.length - 1); if (number_display.length === 0) number_display = "0"; };
+	
+	// Функция проверки наличия десятичного разделителя в конце числа
+	function isCommaLast() { return /\./.test(number_display[number_display.length - 1]) };
+	
+	// Функция вывода переменных на дисплеи
+	function refresh() {
+		document.querySelector(".display-main").value = number_display;
+		document.querySelector(".display-result").value = expression_display;
+		if (number_display.length > 25) document.querySelector(".display-main").style.fontSize = "100%";
+		else if (number_display.length > 14) document.querySelector(".display-main").style.fontSize = "200%";
+		else document.querySelector(".display-main").style.fontSize = "350%";
+	}
+	
+	// Функция инициализации ввода пользователя
+	function initialize_user_input(event) {
+		switch(true) {
+		// Верхняя строка цифр
+			case (event.keyCode >= 48 && event.keyCode <= 57): return String(event.keyCode - 48); break;
+		// Цифры на NumPad-клавиатуре
+			case (event.keyCode >= 96 && event.keyCode <= 105): return String(event.keyCode - 96); break;
+		// Знаки математических операций на обычной и NumPad-клавиатуре
+			case (event.keyCode === 106): return "*"; break;
+			case (event.keyCode === 109): return "-"; break;
+			case (event.keyCode === 107): return "+"; break;
+			case (event.keyCode === 111): return "/"; break;
+			case (event.keyCode === 110): return "."; break;
+			case (event.keyCode === 13): return "="; break;
+			case (event.keyCode === 8): return "delete"; break;
+			case (event.keyCode === 46): return "clear"; break;
+			default: return event.target.dataset.meaning;
+		}
+	}
+	
+	function show_popup() {
+		document.querySelector(".popup-round").style.display = "flex";
+		document.querySelector(".black-wrap").style.display = "block";
+		
+		document.querySelector(".popup-round-button").addEventListener("click", function() {
+			if (document.querySelector(".popup-round-r2").checked) round_type = 2;
+			else if (document.querySelector(".popup-round-r3").checked) round_type = 3;
+			else if (document.querySelector(".popup-round-r4").checked) round_type = 4;
+			
+			document.querySelector(".popup-round").style.display = "none";
+			document.querySelector(".black-wrap").style.display = "none";
+		});
+	}
+	
+	
+	// =================== ОСНОВНАЯ ФУНКЦИЯ: Обработка ввода и вывода ===================
+	
+	
+	function calculate(event) {
+		
+		
+		// Переменная, которая хранит ввод пользователя
+		var user_input = initialize_user_input(event);
+		
+		// Проверяем ввод и вывод
+		switch(true) {
+				
+		// Если при вводе на дисплее была ошибка - очистить дисплей
+			case (!/\d/.test(number_display)):
+				reset();
+				break;
+				
+		// Если вводится цифра
+			case (/\d/.test(user_input)):
+				// В случае, если ранее на дисплее был ноль и опять ввели ноль - новые нули не добавлять, чтобы не получилось "0000"
+				if (number_display === "0" && user_input === "0") number_display = "0";
+				// Если ранее на дисплее был ноль и введена другая цифра - заменить ноль на эту цифру, чтобы не получилось "0123"
+				else if (number_display === "0" && user_input != "0") number_display = user_input;
+				// Простое добавление цифр
+				else number_display += user_input;
+				break;
+				
+		// Если вводится знак операции
+			case (/[\+\-\*\/]/.test(user_input)):
+				// Если ранее введенное число не заканчивается десятичным разделителем
+				if (!isCommaLast()) {
+					// Добавляем к результирующему выражению число и знак операции
+					expression_display += number_display + user_input;
+					// А главный дисплей сбрасываем
+					number_display = "0";
+				}
+				break;
+				
+		// Если вводится десятичный разделителель, проверяем есть ли он уже у нас в числе. Если нет - добавляем
+			case (/\./.test(user_input) && !number_display.match(/\./g)):
+				number_display += ".";
+				break;
+
+		// Если нажата кнопка удаления символа, запускаем функцию deleteLast();
+			case (user_input === "delete"):
+				deleteLast();
+				break;
+
+		// Если нажата кнопка сброса, запускаем функцию reset();
+			case (user_input === "clear"):
+				reset();
+				break;
+				
+		// Если нажата кнопка "равно"
+			case (user_input === "="):
+				// Если ранее введенное число не заканчивается десятичным разделителем
+				if (!isCommaLast()) {
+					// Добавляем к результирующему выражению число
+					expression_display += number_display;
+					// На главный дисплей выводим результат математического выражения, полученного из строки в результирующем дисплее
+					number_display = String(eval(expression_display));
+					// Сбрасываем результирующий дисплей
+					expression_display = "";
+				}
+				
+		}
+		
+		// Если главный дисплей выдал ошибку "NaN" - изменяем эту ошибку на "Divide by zero"
+		if (number_display === "NaN") number_display = "Divide by zero";
+		
+		// Вывод на дисплей переменных после всех операций над ними
+		refresh();
+		
+		document.querySelector("." + event.target.classList[1]).setAttribute("animation", "pulse 200ms ease-in-out");
+	};
+	
+	
+	// ============================= КОНЕЦ ОСНОВНОЙ ФУНКЦИИ =============================
+	
+	
+	// Сброс переменных на начальные значения при старте программы
+	reset();
+	
+	// Получение nod-листа со всем элементами с классом .btn
+	var buttons = document.querySelectorAll(".btn");
+	
+		
+	// Подписка на событие "click" всех элементов в перечне button. Обработчик события: функция "calculate"
+	for (var i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener("click", calculate);
+	}
+	
+	// Обработка события нажатия клавиш клавиатуры. Обработчик тот же, функция "calculate"
+	document.addEventListener("keydown", calculate);
+	
+	document.querySelector(".settings").addEventListener("click", show_popup);
+	
+});
